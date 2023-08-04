98baea6de48e0b3db83bbeec0227f751a2e68f7e
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-04 17:47:57 +0530

    Update variables.tf

--- cluster/variables.tf
+++ cluster/variables.tf
@@ -1,13 +1,25 @@
 #---cluster/variables.tf---
 
-variable "public_subnets" {}
+variable "public_subnets" {
+  default = ["subnet-1", "subnet-2", "subnet-3"]  # Replace with the actual subnet IDs
+}
 
-variable "vpc_id" {}
+variable "vpc_id" {
+  default = "vpc-1234567890"  # Replace with the actual VPC ID
+}
 
-variable "endpoint_private_access" {}
+variable "endpoint_private_access" {
+  default = true  # Replace with your desired value (true or false)
+}
 
-variable "endpoint_public_access" {}
+variable "endpoint_public_access" {
+  default = false  # Replace with your desired value (true or false)
+}
 
-variable "instance_types" {}
+variable "instance_types" {
+  default = ["t2.micro"]  # Replace with your desired instance types
+}
 
-variable "key_pair" {}
+variable "key_pair" {
+  default = "terraform"  # Replace with the name of your key pair
+}
