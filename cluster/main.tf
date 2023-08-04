5fecdc8743c44af2839dbbbbd3bb334724684baf
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-04 21:21:06 +0530

    Update main.tf

--- cluster/main.tf
+++ cluster/main.tf
@@ -101,26 +101,26 @@ resource "aws_iam_role" "krypt0-week22-cluster" {
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::875667920160:policy/AmazonEKSVPCResourceController"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.krypt0-week22.name
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::875667920160:policy/AmazonEKSWorkerNodePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.krypt0-week22-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::875667920160:policy/AmazonEKS_CNI_Policy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.krypt0-week22-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::875667920160:policy/AmazonEC2ContainerRegistryReadOnly"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.krypt0-week22-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::875667920160:policy/AmazonEKSClusterPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.krypt0-week22.name
 }
